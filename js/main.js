77a091eee93b48d9aa0e2937f8b1212167e24303
ahojcn <hanoi_ahoj@icloud.com>
2018-10-06 21:37:43 +0800

    Site updated: 2018-10-06 21:37:43

--- js/main.js
+++ js/main.js
@@ -202,3 +202,9 @@ require([], function (){
     })
 
 })
+
+if($(".instagram").length) {
+        require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(obj) {
+            obj.init();
+        });
+    }
